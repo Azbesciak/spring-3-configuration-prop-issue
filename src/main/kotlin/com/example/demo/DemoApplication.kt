6b77597753ab61359db1f62c2758cfcb397fba9c
Witold Kupś <witold.kups@interlan.pl>
2023-03-07 10:23:38 +0100

    Add example showing issue with @ConfigurationProperty and @Component after update to 3.0.4

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -1,7 +1,10 @@
 package com.example.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Component
+import org.springframework.stereotype.Service
 
 @SpringBootApplication
 class DemoApplication
@@ -9,3 +12,12 @@ class DemoApplication
 fun main(args: Array<String>) {
     runApplication<DemoApplication>(*args)
 }
+
+@Service
+class SomeService(val config: SomeConfig)
+
+@ConfigurationProperties("config")
+@Component
+data class SomeConfig(
+    var property: Int = 0,
+)
